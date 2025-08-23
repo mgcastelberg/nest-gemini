8d5aa7dd9c6808f99227630ec5e18c61a5351caa
ManuxDark <mgcastelberg@hotmail.com>
2025-08-23 00:12:33 -0600

    Mi primera consulta a Gemini

--- src/gemini/gemini.service.ts
+++ src/gemini/gemini.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { BasicPromptDto } from './dtos/basic-prompt.dto';
+import { GoogleGenAI } from "@google/genai";
 
 @Injectable()
 export class GeminiService {
 
-    basicPrompt(basicPromptDto: BasicPromptDto) {
-        console.log(basicPromptDto);
-        return basicPromptDto;
+    private ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+
+    async basicPrompt(basicPromptDto: BasicPromptDto) {
+
+        const response = await this.ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: basicPromptDto.prompt,
+            config: {
+                systemInstruction: "Responde únicamente en español, en formato markdown. Usa negritas de esta forma __",
+            }
+        });
+        console.log(response.text);
+        
+        // console.log(basicPromptDto);
+        return response.text;
     }
 }
+
+// systemInstruction: "You are a helpful assistant."
